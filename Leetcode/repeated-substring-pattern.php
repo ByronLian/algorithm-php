098b4c8ff039e44de97490ebfe2480c996558282
Byron <gordon.byron.lien@gmail.com>
2019-02-25 21:44:08 +0800

    Enhance performance of repeated-substring-pattern.php

--- Leetcode/repeated-substring-pattern.php
+++ Leetcode/repeated-substring-pattern.php
@@ -1,7 +1,7 @@
 <?php
 // https://leetcode.com/problems/repeated-substring-pattern/
-// Runtime: 72 ms, faster than 100.00% of PHP online submissions for Repeated Substring Pattern.
-// Memory Usage: 14.8 MB, less than 100.00% of PHP online submissions for Repeated Substring Pattern.
+// Runtime: 68 ms, faster than 100.00% of PHP online submissions for Repeated Substring Pattern.
+// Memory Usage: 14.9 MB, less than 100.00% of PHP online submissions for Repeated Substring Pattern.
 
 class Solution
 {
@@ -24,11 +24,9 @@ class Solution
         // loop1: $current_sub_str = "a", $current_sub_str repeat = "aaaa", not equals s
         // loop2" $current_sub_str = "ab", $current_sub_str repeat = "abab" equals s
     
-        $current_sub_str = '';
-  
         for ($i = 0; $i < $len; $i++) {
-            $current_sub_str .= $s[$i];
-            $current_sub_str_len = strlen($current_sub_str);
+            $current_sub_str = substr($s, 0, $i+1);
+            $current_sub_str_len = $i+1;
   
             if (str_repeat($current_sub_str, $len / $current_sub_str_len) === $s) {
                 return true;
